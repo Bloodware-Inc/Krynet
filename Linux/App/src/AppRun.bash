fe3f148e247a5f661e381dd000cb0553b67f2f84
ΣΝΛ <78288792+vracci@users.noreply.github.com>
2026-07-03 15:40:58 +0000

    Update AppRun.bash

--- Linux/App/src/AppRun.bash
+++ Linux/App/src/AppRun.bash
@@ -2,6 +2,16 @@ g++ src/krynet_linux_secure.cpp \
   -I sciter/include \
   -L sciter/bin.linux/x64 \
   -lsciter \
+  $(pkg-config --cflags --libs gtk+-3.0) \
+  -ldl \
   -std=c++17 \
-  -O2 \
+  -O3 \
+  -march=native \
+  -flto \
+  -fdata-sections \
+  -ffunction-sections \
+  -Wl,--gc-sections \
+  -Wl,-rpath,'$ORIGIN/../lib:$ORIGIN' \
+  -s \
+  -DNDEBUG \
   -o krynet
